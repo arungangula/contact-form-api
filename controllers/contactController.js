00166755e1f10939c209b831d4aece6a04b48506
arungangula <arungangula93@gmail.com>
2025-07-18 17:51:22 -0700

    sending the contact information to personal gmail

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,9 +1,37 @@
+const nodemailer = require('nodemailer');
 const { validationResult } = require('express-validator');
 
-exports.sendContactEmail = (req, res) => {
+exports.sendContactEmail = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
-    res.json({"msg": "Hello world"});
+    
+    const  { name, email, message } = req.body;
+
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+
+        await transporter.sendMail({
+            from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_USER}>`,
+            to: `${name} <${email}>`,
+            subject: 'New Contact Form Message',
+            text: message,
+            html: `<p><strong>From: </strong> ${name} ${email}</p><br/>
+                    <p><strong>Message: </strong> ${message}</p>
+            `
+        })
+        res.json({message: 'Message sent successfully'});
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({error: 'Failed to send email'});
+    }
+
 }
